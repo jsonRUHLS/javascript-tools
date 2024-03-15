87157d14859a63a14c2863e1d775eceeb47d2faf
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-03-15 17:12:36 -0400

    feat: added functionality for prepending value to front of linked list

--- data-structures/linked-list/linked-list__example.js
+++ data-structures/linked-list/linked-list__example.js
@@ -2,53 +2,29 @@ import {size} from './linked-list__size.function.mjs';
 import {clearLinkedList} from './linked-list__clear.function.mjs';
 import {getFirst} from './linked-list__get-first.function.mjs';
 import {getLast} from './linked-list__get-last.function.mjs';
+import {LinkedList} from './linked-list.class.js';
 
 // example of linked list in javascript
-const list = {
-	head: {
-		value: 6,
-		next: {
-			value: 10,
-			next: {
-				value: 12,
-				next: {
-					value: 3,
-					next: null,
-				},
-			},
-		},
-	},
-};
-console.log(list.head.next.next.value); // 12
+// const list = {
+// 	head: {
+// 		value: 6,
+// 		next: {
+// 			value: 10,
+// 			next: {
+// 				value: 12,
+// 				next: {
+// 					value: 3,
+// 					next: null,
+// 				},
+// 			},
+// 		},
+// 	},
+// };
+// console.log(list.head.next.next.value); // 12
 
-//Path: linkedList.js
-//example of linked list in javascript
-class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-	}
-}
+export const myLinkedList = new LinkedList(10);
 
-class LinkedList {
-	constructor(value) {
-		this.head = {
-			value: value,
-			next: null,
-		};
-		this.tail = this.head;
-		this.length = 1;
-	}
-	append(value) {
-		const newNode = new Node(value);
-		this.tail.next = newNode;
-		this.tail = newNode;
-		this.length++;
-		return this;
-	}
-}
-
-const myLinkedList = new LinkedList(10);
+// insert new nodes at the end of the linked list
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.append(20);
@@ -62,6 +38,10 @@ console.log('getFirst: ', getFirst(myLinkedList));
 // return the last node of the linked list
 console.log('getLast: ', getLast(myLinkedList));
 console.log(myLinkedList);
+
+// insert a node at the front of the linked list
+myLinkedList.prepend(49);
+console.log('after inserting new node to front: ', myLinkedList);
 
 // empty the linked list
 console.log('clear: ', clearLinkedList(myLinkedList));
